b48f7a6a09034be0bb51d17d205dc64090ea9255
dessygil <desmondgilmour@gmail.com>
2022-12-20 00:02:02 -0500

    still working on connecting gtihub API turns out I set it up for nextjs not react so back to square 0

--- client/src/components/homepage/portfolio/Portfolio.jsx
+++ client/src/components/homepage/portfolio/Portfolio.jsx
@@ -1,50 +1,89 @@
-import React, {useEffect, useState} from 'react';
-import { ApolloClient, InMemoryCache, createHttpLink, gql } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import React, { useEffect, useState } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  gql,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import "./portfolio.css";
 
-
-export default function Portfolio() {
+export default function Portfolio({ pinnedItems }) {
+  const gitHubGraphQlSearch = `
+  {
+    viewer {
+      login
+    }
+    user(login: "dessygil") {
+      id
+      pinnedItems(first: 6) {
+        edges {
+          node {
+            ... on Repository {
+              name
+              createdAt
+              description
+              repositoryTopics(last: 10) {
+                edges {
+                  node {
+                    topic {
+                      name
+                    }
+                  }
+                }
+              }
+              stargazerCount
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
 
   useEffect(() => {
     console.log(10);
-  },[]);
-
+    console.log("pinnedItems", pinnedItems);
+  }, []);
 
   return (
     <div className="portfolio">
-      <h2 className="numbered-heading">
-        Portfolio
-      </h2>
-
+      <h2 className="numbered-heading">Portfolio</h2>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(props){
+export async function getStaticProps() {
   const httpLink = createHttpLink({
-    uri: 'https://api.github.com/graphql',
+    uri: "https://api.github.com/graphql",
   });
-  
   const authLink = setContext((_, { headers }) => {
     return {
       headers: {
         ...headers,
         authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-      }
-    }
+      },
+    };
   });
-  
+
+  console.log("I am here");
   const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
 
-  return(
-    props:{
+  const { data } = await client.query({
+    query: gql``,
+  });
 
-    }
-  );
+  const { user } = data;
+  const pinnedItems = user.pinnedItems.edges.map((edge) => edge.node);
 
+  return {
+    props: {
+      pinnedItems,
+    },
+  };
 }
